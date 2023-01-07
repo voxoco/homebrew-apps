5172c6f67363f65571325eb2342033aae6b03db6
homebrew-releaser <homebrew-releaser@example.com>
2023-01-07 04:25:58 +0000

    "Brew formula update for nqlite version v1.4.1"

--- formula/nqlite.rb
+++ formula/nqlite.rb
@@ -5,26 +5,26 @@
 class Nqlite < Formula
   desc "Easy, lightweight, relational database using sqlite and nats jetstream"
   homepage "https://github.com/voxoco/nqlite"
-  url "https://github.com/voxoco/nqlite/archive/v1.3.1.tar.gz"
-  sha256 "4d8c38dbd16c55a46ab1311808ca703520f43baf8c3865a937ef6a76d58f17a4"
+  url "https://github.com/voxoco/nqlite/archive/v1.4.1.tar.gz"
+  sha256 "23f0229da013ec7a55982836d3690d0f49aea19689436633d947bc1e1f09b3a7"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/voxoco/nqlite/releases/download/v1.3.1/nqlite-1.3.1-darwin-amd64.tar.gz"
-      sha256 "d0025335589a70a54a73f3513675fc6dcb95446d066501e6d8127e96cdcf47b4"
+      url "https://github.com/voxoco/nqlite/releases/download/v1.4.1/nqlite-1.4.1-darwin-amd64.tar.gz"
+      sha256 "09135267063447ba2e0542994c3dda1342d60a6b3f28f304096c4646188628b6"
     end
 
     on_arm do
-      url "https://github.com/voxoco/nqlite/releases/download/v1.3.1/nqlite-1.3.1-darwin-arm64.tar.gz"
-      sha256 "6698afc68f677e9af03e613e7b8a3199749d67528faccb4e678990b5f0fbe4de"
+      url "https://github.com/voxoco/nqlite/releases/download/v1.4.1/nqlite-1.4.1-darwin-arm64.tar.gz"
+      sha256 "f80b5cb81d0362a2e41e1da5ed427a82b83a92871c1a9d33a7a44b19f92d772b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/voxoco/nqlite/releases/download/v1.3.1/nqlite-1.3.1-linux-amd64.tar.gz"
-      sha256 "f5006ade4571dc87f4e6da064e4362ef09c0018c8e599cb19b6c5a7a966ee36e"
+      url "https://github.com/voxoco/nqlite/releases/download/v1.4.1/nqlite-1.4.1-linux-amd64.tar.gz"
+      sha256 "dd1e88569a588ad1dada9abd33261e510afaa60b98973b3aaacd117428f5a844"
     end
   end
 
